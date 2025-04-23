106c5c1979d238f95b34b8473bb34583e736d6a6
Biagio Iorio <biorio06@gmail.com>
2025-04-23 22:33:45 +0200

    Spring Boot Project initialized

---
+++ src/test/java/com/biagioiorio/public_ip_logger_api/PublicIpLoggerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biagioiorio.public_ip_logger_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicIpLoggerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
